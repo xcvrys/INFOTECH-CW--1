00961e27c51f26750e85dfb53f35c034d66a0968
Admin <admin@MacBook-Pro-Infotech-86.local>
2022-10-18 16:59:47 +0200

    DataSet

--- backend/db.ts
+++ backend/db.ts
@@ -1,21 +1,21 @@
 import mongoose from "mongoose";
-import {Quiz, Score, Video} from "./models";
+import { Quiz, Score, Video } from "./models";
 
-mongoose.connect("mongodb://admin:admin@localhost:27017/videosAppData?authSource=admin&authMechanism=DEFAULT").then(() => {});
+mongoose.connect("mongodb://admin:admin@localhost:27017/videosAppData?authSource=admin&authMechanism=DEFAULT").then(() => { });
 
 export const getAllVideosPreview = async () => {
-    return Video.find({},{slug:1, title:1, thumb:1, director:1, rating: 1});
+    return Video.find({}, { slug: 1, title: 1, thumb: 1, director: 1, rating: 1 });
 }
 
-export const getSelectedVideo = async (slug:string) => {
-    return Video.findOne({slug:slug},{_id:0});
+export const getSelectedVideo = async (slug: string) => {
+    return Video.findOne({ slug: slug }, { _id: 0 });
 }
 
-export const getSelectedQuestions = async (slug:string) => {
-    return Quiz.findOne({slug:slug},{_id:0,"questions._id":0,"questions.answers._id":0});
+export const getSelectedQuestions = async (slug: string) => {
+    return Quiz.findOne({ for: slug }, { _id: 0, "questions._id": 0, "questions.answers._id": 0 });
 }
 
-export const saveQuizScore = async (slug:string, username:string, score:number) => {
+export const saveQuizScore = async (slug: string, username: string, score: number) => {
     const quizScore = new Score({
         slug: slug,
         username: username,
@@ -24,6 +24,6 @@ export const saveQuizScore = async (slug:string, username:string, score:number) 
     await quizScore.save();
 }
 
-export const getQuizLeaderboard = async (slug:string) => {
-    return Score.find({slug:slug}, {_id:0,__v:0,slug:0}).sort([["score",-1]]);
+export const getQuizLeaderboard = async (slug: string) => {
+    return Score.find({ slug: slug }, { _id: 0, __v: 0, slug: 0 }).sort([["score", -1]]);
 }
